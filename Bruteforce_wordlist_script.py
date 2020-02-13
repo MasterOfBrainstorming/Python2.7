2cb02e13646444b9d2e0b9c8e849892820d4e521
Mikael Romanov <mikael.romanov@hotmail.com>
2020-02-13 20:31:26 +0200

    Update Bruteforce_wordlist_script.py

--- Bruteforce_wordlist_script.py
+++ Bruteforce_wordlist_script.py
@@ -1,25 +1,3 @@
-# This is a bruteforce password list generator where you deside the lenght of the password, password
-# starting point and end point(example aaaaaa-bababa), lowercase, uppercase, numbers and special characters.
-# The program generates the words into a text file that the user defines
-# WARNING lenght that are bigger than 5 strings that have from aaaaa-zzzzz with all atributes
-# cost a large amount of harddisk space and are not considered just waste of space.
-# If you want to use that method you need to just use bruteforce attack strait away.
-# This is merely a test where I test if I manage to pull a somewhat usefull program.
-
-# The reason for this program to exist is that, you can use statitically acquired information to
-# limit the passwords for certain atributes lenght and range
-
-# There is also an option to make a social hacking wordlist of the specific victim,
-# of course you need to use cewl to acquire the information or to manuallyt dig them.
-# This is a good method if you get large amount of information of the victim.
-
-# 1. option for bruteforce list parameters are:
-#       lenght, smallcase/uppercase, numbers, special characters,
-
-# 2. option for social wordlist parameters are:
-#       victims full name,age, music, country, sibling name/nicknames+age, parents full name+age,
-#       bestfriend+nickname, workplace,job,, car, hobbies, nickname,
-
 
 import itertools
 import re
@@ -31,10 +9,7 @@ import string
 class ERROR_USER_INPUT(Exception):
     pass
 
-def brute_attack():
-    pass
-def list_brute_attack():
-    pass
+
 
 def elite_list(passwd):
     elitechr = {"a":4,
@@ -43,13 +18,11 @@ def elite_list(passwd):
                 "i":1,
                 "o":0,
                 "z":2}
-    pass
+    
 
-def social_attack():
-    pass
 
-# This is the answer function that is called through the program. The fucntion just parses the input
-# so that if a user inputs Y or y / N or n as the first character, it will return a 1 or 0
+
+
 def answer():
     try:
         answ = raw_input("Yes/No >")
@@ -61,8 +34,7 @@ def answer():
     except Exception as e:
         print "Wrong input"
 
-# NOTE TO SELF
-# In PROGRESS, implement into the file writing
+
 
 def proggress_bar():
     width = 40
@@ -77,13 +49,6 @@ def proggress_bar():
         sys.stdout.flush()
     sys.stdout.write("\n")
 
-# This is the brutelist that defines the output.
-# First there are objects for lowercase, uppercase, numbers and special characters
-# NOTE TO SELF (could be inside a dictionary)
-
-# Formatting the brute
-
-# The user will be prompted with 4 question where he answers eather yes or no.
 
 def brute_list(lenght):
     chars = "abcdefghijklmnopqrstuvwxyz"
@@ -211,10 +176,7 @@ def makefile():
     except Exception as e:
             print "failed to make a file"
 
-# The Function will ask the user for input the lenght of the pasword, then just compares the output
-# with the input and if it is 6 or over then the user will be prompted with a warning.
-# If the warning returns as 1 then the code will proceed and will send the lenght to the function
-# brute_list
+
 
 def password_lenght():
     answ = int(raw_input("[+] How many characters in the password?\n   >"))
@@ -231,34 +193,21 @@ def password_lenght():
         test = parse_user_range(answ)
         brute_list(answ)
 
-# First thing that pops up, these are the asnwers to the question at start.
-# [Option 1] call the function password leght
-# [Option 2] doesm't do anything at the momement, same goes for the rest of all
-# except the [Option 0] which exits
+
     
 def main():
     
-    print "1=bruteforce list\n2=social list\n3=use the wordlist"\
-              "\n4=brute attack,\n5=social attack\n6=Word combination\n\n0=EXIT"
+    print "1=bruteforce list\n\n0=EXIT"
     try:
         option = int(raw_input("[+] Choose an option:\n   >"))
-        if option in [1,2,3,4,5,6,0]:
+        if option in [1,2,0]:
             if option == 1:
                 dobrutelist = password_lenght()
                 return dobrutelist
             elif option == 2:
                 doelitelist = elite_list()
                 return doelitelist
-            elif option == 3:
-                listbruteattack == list_brute_attack()
-                return dobruteattack
-            elif option == 4:
-                dobruteattack == brute_attack()
-            elif option == 5:
-                dosocialattack == social_attack()
-            elif option == 6:
-                parse_user_range()
-                #dowordcombination == word_combination()
+            
             elif option == 0:
                 exit()
         elif len(option)>2:
